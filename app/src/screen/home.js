e4931a9e2c706f79bd79c10355c4bfbb3f2450a2
Beyazuddin <107599239+Beyazuddin@users.noreply.github.com>
2022-10-05 06:07:37 -0700

    App

---
+++ app/src/screen/home.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import {useNavigate} from "react-router-dom"
+
+export default function Home() {
+  const navigate = useNavigate()
+  const [name, setName] = useState("")
+  const [room, setRoom] = useState("")
+  return (
+    <div className={"Modal Modal_Show"}>
+        <div className="Modal_Con">
+          <input value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter name"/>
+          <input value={room} onChange={(e)=>setRoom(e.target.value)} placeholder="Enter Room"/>
+          <button onClick={()=>{
+            if(name && room){
+              navigate({
+                pathname:"/chat",
+                search: `u=${name}&r=${room}`
+              })
+            }
+          }}>Submit</button> 
+        </div>
+      </div>
+  )
+}
